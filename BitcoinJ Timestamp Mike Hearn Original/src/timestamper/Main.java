a33954c9eec83ff5f9b510f459f52d485d497213
Michael Fehr <javacrypto@gmx.de>
2020-03-30 11:54:31 +0200

    Add files via upload

---
+++ BitcoinJ Timestamp Mike Hearn Original/src/timestamper/Main.java
@@ -0,0 +1,289 @@
+package timestamper;
+
+/*
+    Original source: https://github.com/mikehearn/devcoretalk
+    adopted to use with bitcoinJ 0.15.6
+    see all ### for deleted, changed or added codelines
+    origin uses lib in lib_version_15-8graddle
+    changed uses lib in lib_version 15-6
+
+ */
+
+/*
+ * Herkunft/Origin: http://javacrypto.bplaced.net/
+ * Programmierer/Programmer: Mike Hearn
+ * Copyright/Copyright: Mike Hearn
+ * Lizenttext/Licence: verschiedene Lizenzen / several licenses
+ * e.g. Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * getestet mit/tested with: Java Runtime Environment 11.0.5 x64
+ * verwendete IDE/used IDE: intelliJ IDEA 2019.3.1
+ * Datum/Date (dd.mm.jjjj): 18.03.2020
+ * Programm/Program: BitcoinJ Client WalletTemplate V1
+ * Funktion: zeigt die Erstellung eines Bitcoin Clients mit rudimentaeren Funktionen
+ * Function: show the creating of a bitcoin client with reduced functionality
+ *
+ * Sicherheitshinweis/Security notice
+ * Die Programmroutinen dienen nur der Darstellung und haben keinen Anspruch auf eine korrekte Funktion,
+ * insbesondere mit Blick auf die Sicherheit !
+ * Prüfen Sie die Sicherheit bevor das Programm in der echten Welt eingesetzt wird.
+ * The program routines just show the function but please be aware of the security part -
+ * check yourself before using in the real world !
+ *
+ * Sie benötigen diverse Bibliotheken (alle im Github-Archiv im Unterordner "libs")
+ * You need a lot of libraries (see my Github-repository in subfolder "libs")
+ * verwendete BitcoinJ-Bibliothek / used BitcoinJ Library: bitcoinj-core-0.15.6.jar
+ * my Github-Repository: https://github.com/java-crypto/BitcoinJ
+ * libs in my Github-Repo: https://github.com/java-crypto/BitcoinJ_Libraries
+ *
+ * Source/Basis: https://github.com/mikehearn/devcoretalk
+ * Explanation: https://bitcoinj.github.io/document-timestamp-app
+ * Explanation Video: https://www.youtube.com/watch?v=V_H41AgPZW8
+ */
+
+import com.google.common.util.concurrent.*;
+import javafx.scene.input.*;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.*;
+import org.bitcoinj.utils.BriefLogFormatter;
+import org.bitcoinj.utils.Threading;
+import org.bitcoinj.wallet.DeterministicSeed;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import timestamper.controls.NotificationBarPane;
+import timestamper.utils.GuiUtils;
+import timestamper.utils.TextFieldValidator;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static timestamper.utils.GuiUtils.*;
+
+public class Main extends Application {
+    public static String APP_NAME = "Timestamper";
+
+    // ### public static NetworkParameters params = RegTestParams.get();
+    public static NetworkParameters params = TestNet3Params.get(); // ### new working in Testnet
+    public static WalletAppKit bitcoin;
+    public static Main instance;
+
+    private StackPane uiStack;
+    private Pane mainUI;
+    public MainController controller;
+    public NotificationBarPane notificationBar;
+    public Stage mainWindow;
+
+    @Override
+    public void start(Stage mainWindow) throws Exception {
+        try {
+            realStart(mainWindow);
+        } catch (Throwable e) {
+            GuiUtils.crashAlert(e);
+            throw e;
+        }
+    }
+
+    private void realStart(Stage mainWindow) throws IOException {
+        instance = this;
+        // Show the crash dialog for any exceptions that we don't handle and that hit the main loop.
+        GuiUtils.handleCrashesOnThisThread();
+
+        this.mainWindow = mainWindow;
+
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            // We could match the Mac Aqua style here, except that (a) Modena doesn't look that bad, and (b)
+            // the date picker widget is kinda broken in AquaFx and I can't be bothered fixing it.
+            // AquaFx.style();
+        }
+
+        // Load the GUI. The MainController class will be automagically created and wired up.
+        URL location = getClass().getResource("main.fxml");
+        FXMLLoader loader = new FXMLLoader(location);
+        mainUI = loader.load();
+        controller = loader.getController();
+        // Configure the window with a StackPane so we can overlay things on top of the main UI, and a
+        // NotificationBarPane so we can slide messages and progress bars in from the bottom. Note that
+        // ordering of the construction and connection matters here, otherwise we get (harmless) CSS error
+        // spew to the logs.
+        notificationBar = new NotificationBarPane(mainUI);
+        // ### modified
+        mainWindow.setTitle(APP_NAME + " Mike Hearn Original");
+        // ### mainWindow.setTitle(APP_NAME);
+        uiStack = new StackPane();
+        Scene scene = new Scene(uiStack);
+        TextFieldValidator.configureScene(scene);   // Add CSS that we need.
+        scene.getStylesheets().add(getClass().getResource("wallet.css").toString());
+        uiStack.getChildren().add(notificationBar);
+        mainWindow.setScene(scene);
+
+        // Make log output concise.
+        BriefLogFormatter.init();
+        // Tell bitcoinj to run event handlers on the JavaFX UI thread. This keeps things simple and means
+        // we cannot forget to switch threads when adding event handlers. Unfortunately, the DownloadListener
+        // we give to the app kit is currently an exception and runs on a library thread. It'll get fixed in
+        // a future version.
+        Threading.USER_THREAD = Platform::runLater;
+        // Create the app kit. It won't do any heavyweight initialization until after we start it.
+        setupWalletKit(null);
+
+        if (bitcoin.isChainFileLocked()) {
+            informationalAlert("Already running", "This application is already running and cannot be started twice.");
+            Platform.exit();
+            return;
+        }
+
+        mainWindow.show();
+
+        WalletSetPasswordController.estimateKeyDerivationTimeMsec();
+
+        bitcoin.addListener(new Service.Listener() {
+            @Override
+            public void failed(Service.State from, Throwable failure) {
+                GuiUtils.crashAlert(failure);
+            }
+        }, Platform::runLater);
+        bitcoin.startAsync();
+
+        scene.getAccelerators().put(KeyCombination.valueOf("Shortcut+F"), () -> bitcoin.peerGroup().getDownloadPeer().close());
+    }
+
+    public void setupWalletKit(@Nullable DeterministicSeed seed) {
+        // If seed is non-null it means we are restoring from backup.
+        bitcoin = new WalletAppKit(params, new File("."), APP_NAME + "-" + params.getPaymentProtocolId()) {
+            @Override
+            protected void onSetupCompleted() {
+                // Don't make the user wait for confirmations for now, as the intention is they're sending it
+                // their own money!
+                bitcoin.wallet().allowSpendingUnconfirmedTransactions();
+                Platform.runLater(controller::onBitcoinSetup);
+            }
+        };
+        // Now configure and start the appkit. This will take a second or two - we could show a temporary splash screen
+        // or progress widget to keep the user engaged whilst we initialise, but we don't.
+        if (params == RegTestParams.get()) {
+            bitcoin.connectToLocalHost();   // You should run a regtest mode bitcoind locally.
+        } else if (params == TestNet3Params.get()) {
+            // As an example!
+            // ### no tor bitcoin.useTor();
+            // bitcoin.setDiscovery(new HttpDiscovery(params, URI.create("http://localhost:8080/peers"), ECKey.fromPublicOnly(BaseEncoding.base16().decode("02cba68cfd0679d10b186288b75a59f9132b1b3e222f6332717cb8c4eb2040f940".toUpperCase()))));
+        } else {
+            // ### no tor bitcoin.useTor();
+        }
+        bitcoin.setDownloadListener(controller.progressBarUpdater())
+                .setBlockingStartup(false)
+                .setUserAgent(APP_NAME, "1.0");
+        if (seed != null)
+            bitcoin.restoreWalletFromSeed(seed);
+    }
+
+    private Node stopClickPane = new Pane();
+
+    public class OverlayUI<T> {
+        public Node ui;
+        public T controller;
+
+        public OverlayUI(Node ui, T controller) {
+            this.ui = ui;
+            this.controller = controller;
+        }
+
+        public void show() {
+            checkGuiThread();
+            if (currentOverlay == null) {
+                uiStack.getChildren().add(stopClickPane);
+                uiStack.getChildren().add(ui);
+                blurOut(mainUI);
+                //darken(mainUI);
+                fadeIn(ui);
+                zoomIn(ui);
+            } else {
+                // Do a quick transition between the current overlay and the next.
+                // Bug here: we don't pay attention to changes in outsideClickDismisses.
+                explodeOut(currentOverlay.ui);
+                fadeOutAndRemove(uiStack, currentOverlay.ui);
+                uiStack.getChildren().add(ui);
+                ui.setOpacity(0.0);
+                fadeIn(ui, 100);
+                zoomIn(ui, 100);
+            }
+            currentOverlay = this;
+        }
+
+        public void outsideClickDismisses() {
+            stopClickPane.setOnMouseClicked((ev) -> done());
+        }
+
+        public void done() {
+            checkGuiThread();
+            if (ui == null) return;  // In the middle of being dismissed and got an extra click.
+            explodeOut(ui);
+            fadeOutAndRemove(uiStack, ui, stopClickPane);
+            blurIn(mainUI);
+            //undark(mainUI);
+            this.ui = null;
+            this.controller = null;
+            currentOverlay = null;
+        }
+    }
+
+    @Nullable
+    private OverlayUI currentOverlay;
+
+    public <T> OverlayUI<T> overlayUI(Node node, T controller) {
+        checkGuiThread();
+        OverlayUI<T> pair = new OverlayUI<T>(node, controller);
+        // Auto-magically set the overlayUI member, if it's there.
+        try {
+            controller.getClass().getField("overlayUI").set(controller, pair);
+        } catch (IllegalAccessException | NoSuchFieldException ignored) {
+        }
+        pair.show();
+        return pair;
+    }
+
+    /**
+     * Loads the FXML file with the given name, blurs out the main UI and puts this one on top.
+     */
+    public <T> OverlayUI<T> overlayUI(String name) {
+        try {
+            checkGuiThread();
+            // Load the UI from disk.
+            URL location = GuiUtils.getResource(name);
+            FXMLLoader loader = new FXMLLoader(location);
+            Pane ui = loader.load();
+            T controller = loader.getController();
+            OverlayUI<T> pair = new OverlayUI<T>(ui, controller);
+            // Auto-magically set the overlayUI member, if it's there.
+            try {
+                if (controller != null)
+                    controller.getClass().getField("overlayUI").set(controller, pair);
+            } catch (IllegalAccessException | NoSuchFieldException ignored) {
+                ignored.printStackTrace();
+            }
+            pair.show();
+            return pair;
+        } catch (IOException e) {
+            throw new RuntimeException(e);  // Can't happen.
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        bitcoin.stopAsync();
+        bitcoin.awaitTerminated();
+        // Forcibly terminate the JVM because Orchid likes to spew non-daemon threads everywhere.
+        Runtime.getRuntime().exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ BitcoinJ Timestamp Mike Hearn Original/src/timestamper/MainController.java
@@ -0,0 +1,340 @@
+package timestamper;
+
+// ### import com.subgraph.orchid.*;
+
+import javafx.animation.*;
+import javafx.beans.property.*;
+import javafx.event.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.util.*;
+import org.bitcoinj.core.*;
+import org.bitcoinj.core.listeners.BlocksDownloadedEventListener;
+import org.bitcoinj.core.listeners.DownloadProgressTracker;
+import org.bitcoinj.script.*;
+import org.bitcoinj.store.*;
+import org.bitcoinj.utils.*;
+import org.bitcoinj.wallet.SendRequest;
+import org.bitcoinj.wallet.Wallet;
+import org.fxmisc.easybind.*;
+import timestamper.controls.*;
+import timestamper.utils.*;
+import timestamper.utils.easing.*;
+
+import javax.annotation.Nullable;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+import static timestamper.Main.*;
+import static timestamper.utils.GuiUtils.*;
+
+/**
+ * Gets created auto-magically by FXMLLoader via reflection. The widget fields are set to the GUI controls they're named
+ * after. This class handles all the updates and event handling for the main UI.
+ */
+public class MainController {
+    public HBox controlsBox;
+    public Label balance;
+    public Button sendMoneyOutBtn;
+    public ClickableBitcoinAddress addressControl;
+    public ListView<Proof> pendingProofsList;
+
+    private static class Proof implements Serializable {
+        byte[] tx, partialMerkleTree;
+        Sha256Hash blockHash;
+
+        transient SimpleIntegerProperty depth = new SimpleIntegerProperty();
+        transient String filename;
+
+        public void saveTo(String filename) throws IOException {
+            try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(filename)))) {
+                oos.writeObject(this);
+            }
+        }
+
+        public static Proof readFrom(String filename) throws IOException {
+            try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(filename)))) {
+                return (Proof) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private BitcoinUIModel model = new BitcoinUIModel();
+    private NotificationBarPane.Item syncItem;
+
+    // Called by FXMLLoader.
+    public void initialize() {
+        addressControl.setOpacity(0.0);
+
+        pendingProofsList.setCellFactory(new Callback<ListView<Proof>, ListCell<Proof>>() {
+            @Override
+            public ListCell<Proof> call(ListView<Proof> param) {
+                return new ListCell<Proof>() {
+                    @Override
+                    protected void updateItem(Proof item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setText("");
+                            setGraphic(null);
+                        } else {
+                            setText("Proof for " + item.filename);
+                            ProgressBar bar = new ProgressBar();
+                            bar.progressProperty().bind(item.depth.divide(3.0));
+                            setGraphic(bar);
+                        }
+                    }
+                };
+            }
+        });
+    }
+
+    public void onBitcoinSetup() {
+        model.setWallet(bitcoin.wallet());
+        addressControl.addressProperty().bind(model.addressProperty());
+        balance.textProperty().bind(EasyBind.map(model.balanceProperty(), coin -> MonetaryFormat.BTC.noCode().format(coin).toString()));
+        // Don't let the user click send money when the wallet is empty.
+        sendMoneyOutBtn.disableProperty().bind(model.balanceProperty().isEqualTo(Coin.ZERO));
+        /* ### no tor
+        TorClient torClient = Main.bitcoin.peerGroup().getTorClient();
+        if (torClient != null) {
+            SimpleDoubleProperty torProgress = new SimpleDoubleProperty(-1);
+            String torMsg = "Initialising Tor";
+            syncItem = Main.instance.notificationBar.pushItem(torMsg, torProgress);
+            torClient.addInitializationListener(new TorInitializationListener() {
+                @Override
+                public void initializationProgress(String message, int percent) {
+                    Platform.runLater(() -> {
+                        syncItem.label.set(torMsg + ": " + message);
+                        torProgress.set(percent / 100.0);
+                    });
+                }
+
+                @Override
+                public void initializationCompleted() {
+                    Platform.runLater(() -> {
+                        syncItem.cancel();
+                        showBitcoinSyncMessage();
+                    });
+                }
+            });
+        } else {
+            showBitcoinSyncMessage();
+        }
+         */
+        // ### just this
+        showBitcoinSyncMessage();
+        model.syncProgressProperty().addListener(x -> {
+            if (model.syncProgressProperty().get() >= 1.0) {
+                readyToGoAnimation();
+                if (syncItem != null) {
+                    syncItem.cancel();
+                    syncItem = null;
+                }
+            } else if (syncItem == null) {
+                showBitcoinSyncMessage();
+            }
+        });
+    }
+
+    private void showBitcoinSyncMessage() {
+        syncItem = Main.instance.notificationBar.pushItem("Synchronising with the Bitcoin network", model.syncProgressProperty());
+    }
+
+    public void sendMoneyOut(ActionEvent event) {
+        // Hide this UI and show the send money UI. This UI won't be clickable until the user dismisses send_money.
+        Main.instance.overlayUI("send_money.fxml");
+    }
+
+    public void settingsClicked(ActionEvent event) {
+        Main.OverlayUI<WalletSettingsController> screen = Main.instance.overlayUI("wallet_settings.fxml");
+        screen.controller.initialize(null);
+    }
+
+    public void restoreFromSeedAnimation() {
+        // Buttons slide out ...
+        TranslateTransition leave = new TranslateTransition(Duration.millis(1200), controlsBox);
+        leave.setByY(80.0);
+        leave.play();
+    }
+
+    public void readyToGoAnimation() {
+        // Buttons slide in and clickable address appears simultaneously.
+        TranslateTransition arrive = new TranslateTransition(Duration.millis(1200), controlsBox);
+        arrive.setInterpolator(new ElasticInterpolator(EasingMode.EASE_OUT, 1, 2));
+        arrive.setToY(0.0);
+        FadeTransition reveal = new FadeTransition(Duration.millis(1200), addressControl);
+        reveal.setToValue(1.0);
+        ParallelTransition group = new ParallelTransition(arrive, reveal);
+        group.setDelay(NotificationBarPane.ANIM_OUT_DURATION);
+        group.setCycleCount(1);
+        group.play();
+    }
+
+    public DownloadProgressTracker progressBarUpdater() {
+        return model.getDownloadProgressTracker();
+    }
+
+    public void onTimestampClicked(ActionEvent event) {
+        // Ask the user for the document to timestamp
+        File doc = new FileChooser().showOpenDialog(Main.instance.mainWindow);
+        if (doc == null) return; // User cancelled
+        try {
+            timestamp(doc);
+        } catch (IOException e) {
+            crashAlert(e);
+        } catch (InsufficientMoneyException e) {
+            informationalAlert("Insufficient funds",
+                    "You need bitcoins in this wallet in order to pay network fees.");
+        }
+    }
+
+    private void timestamp(File doc) throws IOException, InsufficientMoneyException {
+        // Hash it
+        // ### Sha256Hash hash = Sha256Hash.hashFileContents(doc);
+        Sha256Hash hash = Sha256Hash.of(doc);
+
+        // Create a tx with an OP_RETURN output
+        Transaction tx = new Transaction(Main.params);
+        tx.addOutput(Coin.ZERO, ScriptBuilder.createOpReturnScript(hash.getBytes()));
+
+        // Send it to the Bitcoin network
+        // ### Main.bitcoin.wallet().sendCoins(Wallet.SendRequest.forTx(tx));
+        // ### new 4 lines
+        SendRequest sendRequest = SendRequest.forTx(tx);
+        Coin coinsFee = Coin.valueOf(1000); // ### decrease the fee from 100000 to 1000
+        sendRequest.feePerKb = coinsFee;  // ### decrease the fee from 100000 to 1000
+        Wallet.SendResult sendResult = Main.bitcoin.wallet().sendCoins(sendRequest);
+
+        // Add it to the UI list
+        Proof proof = new Proof();
+        proof.tx = tx.bitcoinSerialize();
+        proof.filename = doc.toString();
+        pendingProofsList.getItems().add(proof);
+
+        // Grab the merkle branch when it appears in the block chain
+        /* ### new EventListener
+        Main.bitcoin.peerGroup().addEventListener(new AbstractPeerEventListener() {
+            @Override
+            public void onBlocksDownloaded(Peer peer, Block block, FilteredBlock filteredBlock, int blocksLeft) {
+                List<Sha256Hash> hashes = new ArrayList<>();
+                PartialMerkleTree tree = filteredBlock.getPartialMerkleTree();
+                tree.getTxnHashAndMerkleRoot(hashes);
+                if (hashes.contains(tx.getHash())) {
+                    proof.partialMerkleTree = tree.bitcoinSerialize();
+                    proof.blockHash = filteredBlock.getHash();
+                }
+            }
+        });
+        */
+        // new listener
+        Main.bitcoin.peerGroup().addBlocksDownloadedEventListener(new BlocksDownloadedEventListener() {
+            @Override
+            public void onBlocksDownloaded(Peer peer, Block block, @Nullable FilteredBlock filteredBlock, int i) {
+                List<Sha256Hash> hashes = new ArrayList<>();
+                // ### new: try-cause to avoid a Null Pointer Execption
+                try {
+                    PartialMerkleTree tree = filteredBlock.getPartialMerkleTree();
+                    tree.getTxnHashAndMerkleRoot(hashes);
+                    if (hashes.contains(tx.getTxId())) {
+                        proof.partialMerkleTree = tree.bitcoinSerialize();
+                        proof.blockHash = filteredBlock.getHash();
+                    }
+                } catch (NullPointerException e) {
+                    // do nothing
+                }
+            }
+        });
+        // Wait for confirmations (3)
+        tx.getConfidence().addEventListener((confidence, reason) -> {
+            if (confidence.getConfidenceType() != TransactionConfidence.ConfidenceType.BUILDING)
+                return;
+            proof.depth.set(confidence.getDepthInBlocks());
+            if (proof.depth.get() == 3) {
+                // Save the proof to disk
+                String filename = doc.toString() + ".timestamp";
+                try {
+                    proof.saveTo(filename);
+                    // Remove it from the UI list
+                    pendingProofsList.getItems().remove(proof);
+                    // Notify the user that it's done
+                    informationalAlert("Proof complete", "Saved to " + filename);
+                } catch (IOException e) {
+                    crashAlert(e);
+                }
+            }
+        });
+    }
+
+    public void onVerifyClicked(ActionEvent event) {
+        // Ask the user for the document to verify
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Timestamp proofs", "*.timestamp"));
+        File proofFile = chooser.showOpenDialog(Main.instance.mainWindow);
+        if (proofFile == null) return;  // User cancelled.
+
+        try {
+            StoredBlock cursor = verify(proofFile);
+            // Notify the user that the proof is valid and what the timestamp is
+            informationalAlert("Proof valid!", "Document existed at " + cursor.getHeader().getTime());
+        } catch (IOException | BlockStoreException e) {
+            crashAlert(e);
+        } catch (ProofException e) {
+            informationalAlert("Proof was invalid", e.getMessage());
+        }
+    }
+
+    private StoredBlock verify(File proofFile) throws IOException, ProofException, BlockStoreException {
+        // Load the proof file
+        Proof proof = Proof.readFrom(proofFile.getAbsolutePath());
+
+        // Hash the document
+        String docFile = proofFile.getAbsoluteFile().toString().replace(".timestamp", "");
+        // ### Sha256Hash hash = Sha256Hash.hashFileContents(new File(docFile));
+        Sha256Hash hash = Sha256Hash.of(new File(docFile));
+
+        // Verify the hash is in the OP_RETURN output of the tx
+        Transaction tx = new Transaction(Main.params, proof.tx);
+        boolean found = false;
+        for (TransactionOutput output : tx.getOutputs()) {
+            // ### deprecated if (!output.getScriptPubKey().isOpReturn()) continue;
+            if (!ScriptPattern.isOpReturn(output.getScriptPubKey())) continue; // new
+            //noinspection ConstantConditions
+            if (!Arrays.equals(output.getScriptPubKey().getChunks().get(1).data,
+                    hash.getBytes()))
+                throw new ProofException("Hash does not match OP_RETURN output");
+            found = true;
+            break;
+        }
+        if (!found) throw new ProofException("No OP_RETURN output in transaction");
+        // Verify the transaction is in the Merkle proof
+        PartialMerkleTree tree = new PartialMerkleTree(Main.params, proof.partialMerkleTree, 0);
+        List<Sha256Hash> hashes = new ArrayList<>();
+        Sha256Hash merkleRoot = tree.getTxnHashAndMerkleRoot(hashes);
+        // ### deprecated if (!hashes.contains(tx.getHash()))
+        if (!hashes.contains(tx.getTxId())) // ### new
+            throw new ProofException("Transaction not found in Merkle proof");
+
+        // Find the block given the hash
+        StoredBlock cursor = Main.bitcoin.chain().getChainHead();
+        while (cursor != null && !cursor.getHeader().getHash().equals(proof.blockHash)) {
+            cursor = cursor.getPrev(Main.bitcoin.store());
+        }
+        if (cursor == null)
+            throw new ProofException("Could not find given block hash: " + proof.blockHash);
+
+        // Verify the Merkle proof is linked to the block header
+        if (!cursor.getHeader().getMerkleRoot().equals(merkleRoot))
+            throw new ProofException("Merkle root does not match block header");
+        return cursor;
+    }
+
+    private class ProofException extends Exception {
+        public ProofException(String s) {
+            super(s);
+        }
+    }
+}
